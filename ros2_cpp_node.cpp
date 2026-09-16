46f892a2e586201b2ecf48fe5744cec780c7a6be
gshuahh <2154836537@qq.com>
2026-09-16 20:45:06 +0800

    我的第一个ROS2 C++画圆节点

---
+++ ros2_cpp_node.cpp
@@ -0,0 +1,11 @@
+#include "rclcpp/rclcpp.hpp"
+
+int main(int argc,char** argv)
+{
+    rclcpp::init(argc,argv);//initial
+    auto node = std::make_shared<rclcpp::Node>("cpp_node");
+    RCLCPP_INFO(node->get_logger(),"hello c++ node!");
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    return 0;
+}
